ac7009fe4b8a80896191c17b43df481ad7602efc
Rofaeel Wagdy <rofaeelwagdy@gmail.com>
2024-11-03 16:31:48 +0200

    added AdminLogin gui and alertbox

---
+++ src/frontend/AlertBox.java
@@ -0,0 +1,29 @@
+package frontend;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class AlertBox {
+    public static void display(String title, String message) {
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+
+        Label alertMessage = new Label(message);
+
+        Button closeButton = new Button("Close");
+        closeButton.setOnAction(e -> window.close());
+
+        VBox alertLayout = new VBox(20);
+        alertLayout.getChildren().addAll(alertMessage, closeButton);
+
+        Scene alertScene = new Scene(alertLayout, 400, 100);
+        window.setScene(alertScene);
+        window.showAndWait();
+
+    }
+}
